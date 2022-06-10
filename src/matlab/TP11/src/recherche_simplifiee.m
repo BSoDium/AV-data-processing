d14b4d8a9d222e40e5f4b1ce2990ae93814e6377
l3alr0g <negreljerzy.philippe@gmail.com>
2022-06-10 18:42:35 +0200

    feat: merge versions

--- src/matlab/TP11/src/recherche_simplifiee.m
+++ src/matlab/TP11/src/recherche_simplifiee.m
@@ -7,21 +7,19 @@ function resultats = recherche_simplifiee(identifiants, bdd)
   % la bdd est un containers.Map dont chaque clé est un identifiant
 
   % Initialisation des variables
-  cited = zeros(1, length(identifiants));
+  resultats = [];
 
   % Recherche du morceau
   for i = 1:length(identifiants)
 
     if isKey(bdd, identifiants(i))
-      cited(i) = cited(i) + size(bdd(identifiants(i)), 1);
+      occurences = bdd(identifiants(i));
+      occurences = occurences(:, 2);
+      for j = 1:length(occurences)
+        resultats = [resultats, occurences(j)];
+      end
     end
 
   end
-
-  % On choisit le morceau le plus souvent cité
-  [~, indice] = max(cited);
-  cited
-  resultats = bdd(identifiants(indice));
-
 end
-d
+
